5a83eb22c217bff4562d8d756f3c101d636f73ba
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-11 01:12:44 +0200

    added extra 'factors' variable

--- prime_num_checker.py
+++ prime_num_checker.py
@@ -1,13 +1,19 @@
 # prime number checker function
 
+factors = []    # extra code, not important, just for fun
+
 def prime_check():
     is_prime = True
     number = int(input("Enter any positive integer: "))
 
     for num in range(2, number):
         if number % num == 0:
+            factors.append(num)
             is_prime = False
             print(f"{num} divides into {number}")
     if is_prime:
+        factors.append(1)
+        factors.append(number)
         print("Its a bloody prime dawg")
+    print(f"Factors are: {factors}")
 prime_check()
