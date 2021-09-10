872fb26fd2e7db7f9d46dc0271d0699340a1b3d5
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-11 01:05:15 +0200

    added improvements

--- prime_num_checker.py
+++ prime_num_checker.py
@@ -1,7 +1,13 @@
 # prime number checker function
 
-number = 101
+def prime_check():
+    is_prime = True
+    number = int(input("Enter any positive integer: "))
 
-for num in range(2, number):
-    if number % num == 0:
-        print(f"{num} divides into {number}")
+    for num in range(2, number):
+        if number % num == 0:
+            is_prime = False
+            print(f"{num} divides into {number}")
+    if is_prime:
+        print("Its a bloody prime dawg")
+prime_check()
