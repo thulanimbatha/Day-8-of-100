cf3756b8efff6c3fa142136c6d6fd38a524cdd47
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-11 00:34:11 +0200

    changed code to get user input

--- area_calc.py
+++ area_calc.py
@@ -4,5 +4,10 @@
 def num_cans(width, height, coverage):
     area = width * height
     can_num = area / coverage
-    print(can_num, "\n",f"So number of cans needed is: {round(can_num)}")
-num_cans(2, 4, 5)
+    print(can_num, f"\nSo number of cans needed is: {round(can_num)}")
+
+w = int(input("Enter the width of the wall: "))
+h = int(input("Enter the height of the wall: "))
+c = 5
+
+num_cans(width = w, height = h, coverage = c)
