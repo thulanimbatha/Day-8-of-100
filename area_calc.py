c3f0a07e872091b644a28751f86fa67bfa899f09
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-11 00:40:02 +0200

    added correct round up function

--- area_calc.py
+++ area_calc.py
@@ -1,10 +1,11 @@
 # using functions
 # calculate number of cans needed to paint a wall
+import math
 
 def paint_calc(width, height, coverage):
     area = width * height
     num_cans = area / coverage
-    print(num_cans, f"\nSo number of cans needed is: {round(num_cans)}")
+    print(num_cans, f"\nSo number of cans needed is: {math.ceil(num_cans)}")    # round() function doesn't work - we need to round UP (5.4 = 6 not 5.4 = 5)
 
 w = int(input("Enter the width of the wall: "))
 h = int(input("Enter the height of the wall: "))
