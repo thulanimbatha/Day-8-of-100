88b09e0ea91a85eec0752ab5003d913d7521bee0
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-11 00:37:00 +0200

    changed variable names

--- area_calc.py
+++ area_calc.py
@@ -1,13 +1,13 @@
 # using functions
 # calculate number of cans needed to paint a wall
 
-def num_cans(width, height, coverage):
+def paint_calc(width, height, coverage):
     area = width * height
-    can_num = area / coverage
-    print(can_num, f"\nSo number of cans needed is: {round(can_num)}")
+    num_cans = area / coverage
+    print(num_cans, f"\nSo number of cans needed is: {round(num_cans)}")
 
 w = int(input("Enter the width of the wall: "))
 h = int(input("Enter the height of the wall: "))
 c = 5
 
-num_cans(width = w, height = h, coverage = c)
+paint_calc(width = w, height = h, coverage = c)
