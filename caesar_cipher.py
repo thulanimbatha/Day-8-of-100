56fa9698e917ae6ded05552989490840065292ee
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-13 10:59:49 +0200

    fixed function bug

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -11,10 +11,10 @@ shift = int(input("Type the shift number:\n"))
 def encrypt(message, shift):
     cipher_text = ""
     for letter in message:
-        index = alphabet.find(letter)   # find the index of the current letter
-        position = index + shift
-        if position > len(alphabet):    # if INDEXOUTOFBOUNDS error, subtract it by length size
-            position -= 26
+        index = alphabet.find(letter)   # find the index of the current letter - returns INDEX
+        position = index + shift    # [z] = 25, len(alphabet) = 26, 
+        if position >= len(alphabet):    # if IndexError error, subtract it by length size
+            position -= len(alphabet)
         cipher_text += alphabet[position]  # add the new letter to the string
     print(f"The encoded message is: {cipher_text}")
 encrypt(message=message, shift=shift)   # keyword arguments
