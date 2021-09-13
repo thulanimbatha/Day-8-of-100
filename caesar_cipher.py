6b6e201b8624729461819150f43a86a5e332a8b0
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-13 10:29:59 +0200

    fixed IndexOutOfBounds error

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -9,9 +9,12 @@ shift = int(input("Type the shift number:\n"))
 
 '''STEP 1: ENCRYPT FUNCTION'''
 def encrypt(message, shift):
-    shift_letter = ""
+    cipher_text = ""
     for letter in message:
-        index = alphabet.find(letter)
-        shift_letter += alphabet[index + shift]
-    print(f"The message is: {shift_letter}")
-encrypt(message, shift)
+        index = alphabet.find(letter)   # find the index of the current letter
+        position = index + shift
+        if position > len(alphabet):    # if INDEXOUTOFBOUNDS error, subtract it by length size
+            position -= 26
+        cipher_text += alphabet[position]  # add the new letter to the string
+    print(f"The encoded message is: {cipher_text}")
+encrypt(message=message, shift=shift)   # keyword arguments
