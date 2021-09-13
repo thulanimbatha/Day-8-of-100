c8659173e445299ab9e2a053ffd402c463b4c564
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-13 11:19:21 +0200

    added features

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,10 +3,6 @@ import string
 
 alphabet = string.ascii_lowercase
 
-prompt = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-message = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 '''STEP 1: ENCRYPT FUNCTION'''
 def encrypt(message, shift):
     cipher_text = ""
@@ -17,4 +13,28 @@ def encrypt(message, shift):
             position -= len(alphabet)
         cipher_text += alphabet[position]  # add the new letter to the string
     print(f"The encoded message is: {cipher_text}")
-encrypt(message=message, shift=shift)   # keyword arguments
+
+'''STEP 2: DECRYPT FUNCTION'''
+def decrypt(message, shift):
+    cipher_text = ""
+    for letter in message:
+        index = alphabet.find(letter)   # find the index of the current letter - returns INDEX
+        position = index - shift    # [a] = 0, len(alphabet) = 26, [a - 1] = -1 => -1 + 26 = 25 = [z]
+        if position <= alphabet.index("a"):    # if IndexError error, add length size
+            position += len(alphabet)
+        cipher_text += alphabet[position]  # add the new letter to the string
+    print(f"The decoded message is: {cipher_text}")
+
+'''PROGRAM START'''
+prompt = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+if prompt.lower() == "encode":
+    message = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    encrypt(message=message, shift=shift)
+elif prompt.lower() == "decode":
+    message = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    decrypt(message=message, shift=shift)
+else:
+    print("Invalid input!")
